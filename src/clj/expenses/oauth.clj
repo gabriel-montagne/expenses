174f4b38dc63ec3efc3d1ecc47b52e35a9a1d65d
Gabriel Munteanu <gabimunteanu.sdt@gmail.com>
2018-02-19 21:34:32 +0100

    feat(oauth) Add google oauth

--- src/clj/expenses/oauth.clj
+++ src/clj/expenses/oauth.clj
@@ -4,14 +4,23 @@
             [mount.core :refer [defstate]]
             [clojure.tools.logging :as log]))
 
-(defstate consumer
+(defstate twitter-consumer
   :start (oauth/make-consumer
-           (env :oauth-consumer-key)
-           (env :oauth-consumer-secret)
-           (env :request-token-uri)
-           (env :access-token-uri)
-           (env :authorize-uri)
+           (env :oauth-twitter-consumer-key)
+           (env :oauth-twitter-consumer-secret)
+           (env :request-twitter-token-uri)
+           (env :access-twitter-token-uri)
+           (env :authorize-twitter-uri)
            :hmac-sha1))
+
+(defstate google-consumer
+          :start (oauth/make-consumer
+                   (env :oauth-google-consumer-key)
+                   (env :oauth-google-consumer-secret)
+                   (env :request-google-token-uri)
+                   (env :access-google-token-uri)
+                   (env :authorize-google-uri)
+                   :hmac-sha1))
 
 (defn oauth-callback-uri
   "Generates the oauth request callback URI"
@@ -23,13 +32,13 @@
   [request]
   (let [callback-uri (oauth-callback-uri request)]
     (log/info "Fetching request token using callback-uri" callback-uri)
-    (oauth/request-token consumer (oauth-callback-uri request))))
+    (oauth/request-token twitter-consumer (oauth-callback-uri request))))
 
 (defn fetch-access-token
   [request_token]
-  (oauth/access-token consumer request_token (:oauth_verifier request_token)))
+  (oauth/access-token twitter-consumer request_token (:oauth_verifier request_token)))
 
 (defn auth-redirect-uri
   "Gets the URI the user should be redirected to when authenticating."
   [request-token]
-  (str (oauth/user-approval-uri consumer request-token)))
+  (str (oauth/user-approval-uri twitter-consumer request-token)))
